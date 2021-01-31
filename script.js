aaef52c9eb21292a17383725f6bd9b56f53f35f0
Ola Manczak <manczakola@gmail.com>
2021-01-31 15:40:02 +0100

    update localStorage shoppingCart

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ const shoppingCartItems = document.querySelector(".shopping-cart-items");
 const shoppingCartDropdown = document.querySelector(".shopping-cart");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 const totalMoney = document.querySelector(".lighter-text");
-const badge = document.querySelector(".badge");
+const badgeInShoppingCart = document.querySelector(".badge");
 const shoppingCartBtn = document.querySelector("#dropdownMenuButton");
-const numberOfItems = document.querySelector("number");
+const numberOfItems = document.querySelector(".number");
 let money = [];
+let badge = 0;
+const items = JSON.parse(localStorage.getItem('item')) || [];
+const itemsLength = items.length;
+
 
 /// images variables
 const bigImage = document.querySelector("div.big-image > img");
@@ -59,59 +63,74 @@ class Plants {
     }
 
     ///// Functions/////
+    addtoLocalStorage() {
+        const localStorageShoppingCart = JSON.parse(localStorage.getItem('item'));
+
+
+
+        if (localStorageShoppingCart) {
+            for (let i = 0; i < localStorageShoppingCart.length; i++) {
+
+                shoppingCartItems.innerHTML +=
+                    `
+                <li class="clearfix">
+                <img src=${localStorageShoppingCart[i].img} />
+                <span class="item-name">${localStorageShoppingCart[i].name}</span>
+                <span class="item-price">${localStorageShoppingCart[i].value}$</span>
+                <span class='cancelItem btn btn-outline'></span>
+                </li>
+                `;
+
+                money.push(localStorageShoppingCart[i].value);
+                badge++;
+
+                let total = money.reduce(function (a, b) {
+                    return a + b;
+                });
+
+
+                totalMoney.innerText = `Total: ${total}$`;
+                badgeInShoppingCart.innerText = badge;
+                numberOfItems.innerText = badge;
+
+
+            };
+        } else {
+            return
+        }
+
+    }
 
 
     addToShoppingCart(e) {
+
         const shoppingCart = new Object(); //create new object when click on 'add to chart' btn
-        shoppingCart.value = parseFloat(productPrice.innerText);
+        shoppingCart.value = parseInt(productPrice.innerText);
         shoppingCart.name = productName.innerText;
         shoppingCart.img = `images/${productName.innerText.toLowerCase()}1.jpg`;
         // download values of this object
 
-        // add li items on shopping cart
-        shoppingCartItems.innerHTML += `
-        <li class="clearfix">
-        <img src=${shoppingCart.img} />
-        <span class="item-name">${shoppingCart.name}</span>
-        <span class="item-price">${shoppingCart.value}$</span>
-        <span class='cancelItem btn btn-outline'></span>
-        </li>
-        `;
-        window.scrollTo(0, 0);
-        document.querySelector('.navbar-toggler').classList.remove('collapsed');
-        document.querySelector("#navbarToggler").classList.add('show');
-        shoppingCartBtn.classList.add('show');
+
+        // create object in local storage
+        localStorage.setItem('item', JSON.stringify(shoppingCart));
+        var parsItem = JSON.parse(localStorage.getItem('item'));
+
+        // push object to array
+
+        if (items) {
+            items.push(parsItem);
+        } else {
+            return items;
+        }
+
+        // set array of objects to local storage
+        localStorage.setItem('item', JSON.stringify(items));
+
+        // call function of add parsItem to shopping cart
+        plants.listOfObject(items, shoppingCartItems);
+
         shoppingCartBtn.parentNode.children[0].children[0].innerText++; // change number of cart items
 
-        // remove items on x btn
-
-        const cancelItem = document.querySelectorAll("span.cancelItem.btn.btn-outline");
-
-        cancelItem.forEach(btn => btn.addEventListener('click', (e) => {
-
-            let cancelValue = parseFloat(e.target.parentNode.children[2].innerText); // value in $ of cancel item
-            e.target.parentNode.remove(); // remove li item
-            dropdownMenu.classList.add('show'); // dont hide ul list of shopping cart when remove items
-            shoppingCartBtn.parentNode.children[0].children[0].innerText--; // change number of cart items
-            money.pop(cancelValue); // remove $$ from money array
-
-            if (money.length > 0) {
-                var total = money.reduce(function (a, b) {
-                    return a + b;
-                });
-            };
-
-            if (money.length == 0) {
-                totalMoney.innerText = `Total:0$`;
-                badge.innerText = `0`;
-                dropdownMenu.classList.remove('show'); // when nothing is in the shopping cart, hide ul list
-            } else {
-                totalMoney.innerText = `Total: ${total}$`;
-                badge.innerText = `${money.length}`;
-            }
-        }));
-
-        dropdownMenu.classList.add('show'); // show ul list of shopping cart when remove the items
 
         // total money counting when add items to shopping cart
 
@@ -123,11 +142,72 @@ class Plants {
 
 
         totalMoney.innerText = `Total: ${total}$`;
-        badge.innerText = `${money.length}`;
+        badgeInShoppingCart.innerText = `${money.length}`;
 
-
-
+        plants.removeFromShoppingCart(e)
     };
+
+    // add li items on shopping cart
+
+    listOfObject(arr = [], objectList) {
+        objectList.innerHTML = arr.map((plate, i) => {
+            return `
+            <li class="clearfix">
+            <img src=${items[i].img} />
+            <span class="item-name">${items[i].name}</span>
+            <span class="item-price">${items[i].value}$</span>
+            <span class='cancelItem btn btn-outline'></span>
+            </li>
+            `
+        }).join('')
+    }
+
+    removeFromShoppingCart(e) {
+
+        const cancelItem = document.querySelectorAll('.cancelItem');
+
+
+
+        // dont hide ul list of shopping cart when remove items
+        document.getElementById("dropdownMenuButton").classList.add('show')
+        document.querySelector(".dropdown-menu").classList.add('show');
+
+
+        // remove items on 'trash' btn
+
+        cancelItem.forEach(btn => btn.addEventListener('click', (e) => {
+
+            // value in $ of cancel item
+            let cancelValue = parseFloat(e.target.parentNode.children[2].innerText);
+
+            e.target.parentNode.remove(); // remove li item
+            items.pop(e.target.parentNode) //remove item from items array
+            localStorage.setItem('item', JSON.stringify(items)); // update localStorage
+
+
+            // change number of cart items
+            shoppingCartBtn.parentNode.children[0].children[0].innerText--;
+
+            money.pop(cancelValue); // remove $$ from money array
+
+            if (money.length > 0) {
+                var total = money.reduce(function (a, b) {
+                    return a + b;
+                });
+            };
+
+            if (money.length == 0) {
+                totalMoney.innerText = `Total:0$`;
+                badgeInShoppingCart.innerText = `0`;
+                 dropdownMenu.classList.remove('show'); // when nothing is in the shopping cart, hide ul list
+            } else {
+                totalMoney.innerText = `Total: ${total}$`;
+                badgeInShoppingCart.innerText--;
+            }
+        }));
+
+
+    }
 
 
     /// function changing the image product
@@ -223,10 +303,9 @@ class Plants {
         } else {
             bigImage.src = `images/${imageName}1.jpg`;
         }
-        console.log(bigImage.src);
     }
 
-   previousPhoto = (e) => {
+    previousPhoto = (e) => {
 
 
         let imageName = productName.textContent.toLowerCase();
@@ -240,12 +319,14 @@ class Plants {
         }
     }
 
+
+
 }
 const plants = new Plants();
-const bonsai = new Plants('Bonsai', '99,99$', 'images/bonsai3.jpg', 0);
-const succulent = new Plants('Succulent', '19,99$', 'images/succulent1.jpg', 1);
-const terranium = new Plants('Terranium', '59,99$', 'images/terranium1.jpg', 2);
-const cactus = new Plants('Cactus', '29,99$', 'images/cactus1.jpg', 3);
+const bonsai = new Plants('Bonsai', '99$', 'images/bonsai3.jpg', 0);
+const succulent = new Plants('Succulent', '19$', 'images/succulent1.jpg', 1);
+const terranium = new Plants('Terranium', '59$', 'images/terranium1.jpg', 2);
+const cactus = new Plants('Cactus', '29$', 'images/cactus1.jpg', 3);
 
 let planties = [bonsai, succulent, terranium, cactus];
 
@@ -259,17 +340,20 @@ let planties = [bonsai, succulent, terranium, cactus];
 
 
 ///// Event listeners /////
+document.addEventListener('DOMContentLoaded', plants.addtoLocalStorage);
+document.addEventListener('DOMContentLoaded', plants.removeFromShoppingCart);
+
 
 btnShopping.forEach((btn) =>
     btn.addEventListener("click", plants.addToShoppingCart));
 
 // event to change pictures on click in main section    
 smallImages.forEach(img => img.addEventListener('click', (e) => {
-
     const srcOfClickImage = img.children[0].src;
-
     bigImage.src = srcOfClickImage;
 }));
+shoppingCartBtn.addEventListener('click',shoppingCartBtn.style.boxShadow = 'none')
+
 
 // Change image listeners // 
 succulentImage.addEventListener('click', succulent.footerImages);
@@ -283,7 +367,3 @@ previous.addEventListener('click', plants.previousPhoto);
 succulentTitle.addEventListener('touchstart', succulent.footerImages);
 cactusTitle.addEventListener('touchstart', cactus.footerImages);
 terraniumTitle.addEventListener('touchstart', terranium.footerImages);
-
-
-
-
